47ce1bbc7185e2642d8048b0f7cbed0b787e1597
Purnendu Bhowmik Shuvro <bhowmikpurnendu1@gmail.com>
2025-06-30 05:55:05 +0600

    Create JS_3.js

---
+++ JS_3.js
@@ -0,0 +1,8 @@
+const person = {
+name : " Alice ",
+age: 30
+};
+console .log ( person ); // Output : { name : " Alice ", age : 30 }
+
+const numbers = [1, 2, 3, 4];
+console .log ( numbers ); // Output : [1, 2, 3, 4]
